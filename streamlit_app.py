719fbb55f95a5b31b54724f4a76d2b50921108c8
Peter Ierasts <118218663+pierasts@users.noreply.github.com>
2023-01-26 13:43:50 -0500

    Update streamlit_app.py
    Added stop statement to prevent further rows from being added while we debug
    Added URLError library

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -35,6 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# Don't run anyhting past here while we troubleshoot
+streamlit.stop()
 
 # Query Snowflake trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
