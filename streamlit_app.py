d8bb67ff0a7a49538ea6c181a2adf9ed686c7c64
Peter Ierasts <118218663+pierasts@users.noreply.github.com>
2023-01-26 13:34:09 -0500

    Update streamlit_app.py
    Added second text entry box to add new fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,23 +23,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New section to display fruityvice API response
+# New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#Take the json version of the response and normalize it
+# Take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-#Output it to the screen as a table
+# Output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-#Query Snowflake trial account metadata
+# Query Snowflake trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
